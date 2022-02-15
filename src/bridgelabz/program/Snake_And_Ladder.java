a8b6e8afb2116e565e2ac14bf45bbfa2d8da9b3d
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-16 03:20:39 +0530

    Game for single player is initialised.

---
+++ src/bridgelabz/program/Snake_And_Ladder.java
@@ -0,0 +1,25 @@
+package bridgelabz.program;
+
+import java.util.Random;
+
+class gamePlayer {
+	
+    int currentPosition = 0;
+    
+    Random rand = new Random();
+    
+    int attempts = 0;
+    
+    final int noPlay = 0, ladder = 1, snake = 2;
+    
+    int endGame = 0;
+
+}
+
+public class Snake_And_Ladder {
+
+    public static void main(String[] args) {
+    	
+        System.out.println("welcome to snake & ladder game");
+    }
+}

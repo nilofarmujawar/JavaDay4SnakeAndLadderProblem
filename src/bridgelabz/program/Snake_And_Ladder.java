b0d1fabd48c73cf41f5bd858b8e355459b96709a
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-16 03:28:52 +0530

    the player rolls the die to get a no btw 1 to 6.

---
+++ src/bridgelabz/program/Snake_And_Ladder.java
@@ -0,0 +1,22 @@
+package bridgelabz.program;
+
+import java.util.Random;
+
+class gamePlayer {
+	
+    int currentPosition = 0;
+    
+    Random rand = new Random();
+    
+    int dieRoll = rand.nextInt(6)+1;
+    
+}
+
+public class Snake_And_Ladder {
+	
+    public static void main(String[] args) {
+    	
+        System.out.println("welcome to snake & ladder game");
+        
+    }
+}
